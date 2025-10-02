75047d797c3582df2734a10bb396144f1848c662
🅽ɨӄօʍʊ|Nieznany|Ferro| <19400681+anonymousik@users.noreply.github.com>
2025-10-02 15:52:06 +0200

    Create ads.js
    Signed-off-by: 🅽ɨӄօʍʊ|Nieznany|Ferro| <19400681+anonymousik@users.noreply.github.com>

---
+++ js/ads.js
@@ -0,0 +1,11 @@
+// /js/ads.js
+// This file is designed to be blocked by adblockers.
+// If it is blocked, we know an adblocker is running.
+
+var canRunAds = true;
+
+function displayAd() {
+    console.log("This is an advertisement banner.");
+}
+
+displayAd();
